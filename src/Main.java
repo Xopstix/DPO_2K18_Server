7d0c617abee5b4a54cb678c9f256f79c4f900463
manuelsahun2015 <manuel.sahun.2015@salleurl.edu>
2018-03-14 16:22:06 +0100

    Creada la BBDD y implementada, pequeña prueba de demostracion en el main.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import utility.ConectorDB;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
- * Created by xaviamorcastillo on 12/3/18.
+ * Created by xavipargela on 12/3/18.
  */
 public class Main {
-    private int num;
+    public static void main(String[] args) {
+
+        //Gestion BBDD
+        ResultSet prueba;
+
+        //Login BBDD
+        ConectorDB conn = new ConectorDB("adminOrg", "cartofen", "organizerDB", 8889);
+        conn.connect();
+
+        prueba = conn.selectQuery("SELECT * FROM usuarios");
+
+        try {
+            //Recorremos toda la tabla de usuarios de la BBDD
+            while (prueba.next())
+            {
+                //Especificamente le ponenmos que campos queremos leer de la BBDD
+                System.out.println (prueba.getObject("Login") + " " + prueba.getObject("Contraseña"));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Problema al recuperar les dades...");
+        }
+        //Desconexion BBDD
+        conn.disconnect();
+
+    }
 }
